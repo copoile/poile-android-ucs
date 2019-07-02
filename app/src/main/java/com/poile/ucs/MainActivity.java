206034d8fab3d39e1e35901911c221009c3b8557
yaohw007 <726856005@qq.com>
2019-07-02 17:57:19 +0800

    ....

--- app/src/main/java/com/poile/ucs/MainActivity.java
+++ app/src/main/java/com/poile/ucs/MainActivity.java
@@ -2,13 +2,36 @@ package com.poile.ucs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnToLogin;
+
+    private Button mBntH5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnToLogin=findViewById(R.id.bnt_login);
+        mBntH5=findViewById(R.id.bnt_h5);
+        mBtnToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBntH5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/poile/ucs/WebViewActivity.java
@@ -0,0 +1,32 @@
+package com.poile.ucs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        webView=findViewById(R.id.wv_1);
+        webView.setWebViewClient(new WebViewClient(){
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+                }
+            );
+        WebSettings settings = webView.getSettings();
+        //支持js
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/html/index.html");
+    }
+}
